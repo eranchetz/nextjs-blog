893f5a9d086b7db91f06319b2fd7d9e3e324e9be
eranchetz <gitlab@chetz.org>
2020-05-08 15:26:11 +0300

    customize

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p> <q>For me, money is not my definition of success. Inspiring people is a definition of success.
+/ Kanye West</q>
+       </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
